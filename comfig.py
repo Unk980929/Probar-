c9f6d81891f0ef5b031b2897b8edd1a06242e8fa
Pubgne <101445038+Pubgne@users.noreply.github.com>
2022-04-09 22:35:12 -0400

    Add files via upload

---
+++ comfig.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = '5194931812:AAG6tKUEHCjmAstnhCIunU1qNXIaqhOFEgs'
+API_ID = '16536294'
+API_HASH = '0c780f6db3124fafbdc3dcde437e6f21'
+ADMIN_ID = [1759969205]
+
+MOODLE_URL = 'https://evea.uh.cu/'
+MOODLE_USER = 'darian.borges@estudiantes.fbio.uh.cu'
+MOODLE_PASSWORD = 'darian1995'
+PARTES_MB = 100
+

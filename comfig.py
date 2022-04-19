c93397d50792eb7839086212868ae95a2e31a911
Unk980929 <102277711+Unk980929@users.noreply.github.com>
2022-04-19 03:18:32 -0400

    Update comfig.py

--- comfig.py
+++ comfig.py
@@ -1,9 +1,9 @@
 BOT_TOKEN = '5215464007:AAGVXYhjQMeNPI2U3pEJE-gU_wdarz6Yrzg'
-API_ID = 19919438
+API_ID = '19919438'
 API_HASH = 'c6ef54eb0cdd29441ae33921531cb4e8'
 ADMIN_ID = [983731245]
 
-MOODLE_URL = 'moodle.uclv.edu.cu'
+MOODLE_URL = 'https://moodle.uclv.edu.cu'
 MOODLE_USER = 'joborges'
 MOODLE_PASSWORD = 'liany.29'
 PARTES_MB = 300

642ced02188597c7776c8b3bd58cbc73536ade7a
Unk980929 <102277711+Unk980929@users.noreply.github.com>
2022-04-22 10:13:24 -0400

    Update comfig.py

--- comfig.py
+++ comfig.py
@@ -3,7 +3,7 @@ API_ID = '19919438'
 API_HASH = 'c6ef54eb0cdd29441ae33921531cb4e8'
 ADMIN_ID = [983731245]
 
-MOODLE_URL = 'aulacened.uci.cu'
+MOODLE_URL = 'https://aulacened.uci.cu/'
 MOODLE_USER = 'beatriz-chang'
 MOODLE_PASSWORD = 'Chidalgoa*98'
 PARTES_MB = 200

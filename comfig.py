52f50106167a516d8d217544f12663d6c2cc0706
Unk980929 <102277711+Unk980929@users.noreply.github.com>
2022-04-22 10:12:43 -0400

    Update comfig.py

--- comfig.py
+++ comfig.py
@@ -3,8 +3,8 @@ API_ID = '19919438'
 API_HASH = 'c6ef54eb0cdd29441ae33921531cb4e8'
 ADMIN_ID = [983731245]
 
-MOODLE_URL = 'https://moodle.uclv.edu.cu'
-MOODLE_USER = 'joborges'
-MOODLE_PASSWORD = 'liany.29'
-PARTES_MB = 300
+MOODLE_URL = 'aulacened.uci.cu'
+MOODLE_USER = 'beatriz-chang'
+MOODLE_PASSWORD = 'Chidalgoa*98'
+PARTES_MB = 200
 

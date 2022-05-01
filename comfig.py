55b27fb754b087a4770884fc4790caa589955f9c
Unk980929 <102277711+Unk980929@users.noreply.github.com>
2022-05-01 16:16:55 -0400

    Update comfig.py

--- comfig.py
+++ comfig.py
@@ -1,10 +1,10 @@
-BOT_TOKEN = '5346211856:AAEbFPpQwF4g1DcrM2T5WqA57Moz_rVrh_E'
+BOT_TOKEN = '5345338699:AAGBBqRf3l7vWfpkOcagJ70OZMvEaIjgda8'
 API_ID = '19919438'
 API_HASH = 'c6ef54eb0cdd29441ae33921531cb4e8'
 ADMIN_ID = [983731245]
 
-MOODLE_URL = 'https://aulacened.uci.cu/'
-MOODLE_USER = 'beatriz-chang'
-MOODLE_PASSWORD = 'Chidalgoa*98'
+MOODLE_URL = 'https:// evea.uh.cu/'
+MOODLE_USER = 'darian.borges@estudiantes.fbio.uh.cu'
+MOODLE_PASSWORD = 'darian1995'
 PARTES_MB = 200
 
